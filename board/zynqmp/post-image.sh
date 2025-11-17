915d650d5cfc3444cfdf729391e6f6a9d82622f1
Artem Anogda <anogdaa@gmail.com>
2025-11-17 17:21:08 +0200

    upd

--- board/zynqmp/post-image.sh
+++ board/zynqmp/post-image.sh
@@ -4,12 +4,12 @@
 # let's use a symlink with that name that points to the *first*
 # devicetree listed in the config.
 
-FIRST_DT=$(sed -nr \
-               -e 's|^BR2_LINUX_KERNEL_INTREE_DTS_NAME="(xilinx/)?([-_/[:alnum:]\\.]*).*"$|\2|p' \
-               "${BR2_CONFIG}")
+# FIRST_DT=$(sed -nr \
+#                -e 's|^BR2_LINUX_KERNEL_INTREE_DTS_NAME="(xilinx/)?([-_/[:alnum:]\\.]*).*"$|\2|p' \
+#                "${BR2_CONFIG}")
 
-[ -z "${FIRST_DT}" ] || ln -fs "${FIRST_DT}.dtb" "${BINARIES_DIR}/system.dtb"
+# [ -z "${FIRST_DT}" ] || ln -fs "${FIRST_DT}.dtb" "${BINARIES_DIR}/system.dtb"
 
-BOARD_DIR="$(dirname "$0")"
+# BOARD_DIR="$(dirname "$0")"
 
-support/scripts/genimage.sh -c "${BOARD_DIR}/genimage.cfg"
+# support/scripts/genimage.sh -c "${BOARD_DIR}/genimage.cfg"
